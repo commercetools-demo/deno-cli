1cd2eb91eade7e9d18e76553d4f20a1128f8b371
willem-haring <willem.haring@commercetools.com>
2023-06-01 15:43:37 +0200

    fiddling with views

--- commands/view.ts
+++ commands/view.ts
@@ -47,7 +47,7 @@ class denoLoader extends nunjucks.Loader implements nunjucks.ILoader {
 
   getSource(name: string, callback: nunjucks.Callback<Error, nunjucks.LoaderSource>): void | nunjucks.LoaderSource {
     const path = this.path + name
-    
+    console.log(`loading view: ${path}`)
     Deno.readTextFile(path).then((data) => {
       callback(null, {
         src: data,
--- helpers/project/project.ts
+++ helpers/project/project.ts
@@ -1,17 +1,48 @@
 import { Project, sdk } from "../../deps.ts";
 
 export async function project(): Promise<Project> {
+  const res = await new ProjectModel().get()
+  //console.log(res)
+  return res
   try {
-    const handle = await sdk.init();
-    const projectKey = handle.projectKey;
+    const handle = sdk.init();
     const result = await handle
       .apiRoot()
-      .withProjectKey({ projectKey })
+      .withProjectKey({ projectKey: handle.projectKey })
       .get()
       .execute();
     return result.body;
   }
   catch (_error) {
     return undefined
+  }
+}
+
+abstract class baseModel<T>  {
+  private handle!: sdk; 
+  protected root: any
+  constructor() {
+    //console.log('abstract model constructor')
+    this.handle = sdk.init()
+    this.root = this.handle
+      .apiRoot()
+      .withProjectKey({ projectKey: this.handle.projectKey })
+  }
+
+  abstract get(): Promise<T> 
+}
+
+class ProjectModel<T> extends baseModel<T> {
+  
+  async get(): Promise<Project> {
+    try {
+      const result = await this.root
+        .get()
+        .execute()
+      return result.body
+    }
+    catch (_error) {
+      return undefined
+    }
   }
 }
